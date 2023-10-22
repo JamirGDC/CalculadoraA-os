5f34c967d38fb6c1c7e195aaee6cfca67839ec9e
JamirGDC <gutierrez.dc@icloud.com>
2023-10-22 13:47:29 +0200

    agrego funciones

--- app.js
+++ app.js
@@ -1,38 +1,44 @@
-const dayInputByUser = document.querySelector("#day");
-const monthInputByUser = document.querySelector("#month");
-const yearInputByUser = document.querySelector("#year");
+function obtainDay() {
+    const day = document.querySelector("#day").value;
+    return parseInt(day);
+}
+function obtainMonth() {
+    const month = document.querySelector("#month").value;
+    return parseInt(month);
 
-const calculateButton = document.querySelector(".btn-calculate");
-const answerContainer = document.querySelector(".answer");
-
-const outYear = document.querySelector(".outYear");
-const outMonth = document.querySelector(".outMonth");
-const outDay = document.querySelector(".outDay");
-
+}
+function obtainYear() {
+    const year = document.querySelector("#year").value;
+    return parseInt(year);
+}
 
 function calculateDate() {
 
-    const dayParsed = parseInt(dayInputByUser.value);
-    const monthParsed = parseInt(monthInputByUser.value);
-    const yearParsed = parseInt(yearInputByUser.value);
+    const dayInputByUser = obtainDay();
+    const monthInputByUser = obtainMonth();
+    const yearInputByUser = obtainYear();
 
-    if (dayParsed <= 0 || monthParsed <= 0 || yearParsed < 0) {
+    if (dayInputByUser >= 1 && dayInputByUser <= 31 || monthInputByUser >= 1 && monthInputByUser <= 12 || yearInputByUser > 0) {
+        const actualDate = new Date();
+
+        const userDate = new Date(yearInputByUser, monthInputByUser - 1, dayInputByUser);
+        const answerContainer = document.querySelector(".answer");
+
+
+
+        const years = Math.abs(actualDate.getFullYear() - userDate.getFullYear());
+        const actualMonth = actualDate.getMonth();
+        const userMonth = userDate.getMonth();
+        const months = Math.abs(actualMonth - userMonth + (actualDate.getDate() < userDate.getDate() ? -1 : 0));
+        const days = Math.abs(actualDate.getDate() - userDate.getDate());
+
+        answerContainer.innerHTML = `<p><span class="outYear"> ${years} </span>years</p> <p><span class="outMonth">${months} </span>months</p> <p><span class="outDay">${days} </span>days</p>`;
+    }
+    else {
         answerContainer.innerHTML = "Please verify the values entered";
         return;
     }
 
-    const actualDate = new Date();
 
-    const userDate = new Date(yearParsed, monthParsed - 1, dayParsed);
 
-    const timeDiff = actualDate - userDate;
-
-    const years = Math.abs(actualDate.getFullYear() - userDate.getFullYear());
-    const actualMonth = actualDate.getMonth();
-    const userMonth = userDate.getMonth();
-    const months = Math.abs(actualMonth - userMonth + (actualDate.getDate() < userDate.getDate() ? -1 : 0));
-    const days = Math.abs(actualDate.getDate() - userDate.getDate());
-
-    answerContainer.innerHTML = `<p><span class="outYear"> ${years} </span>years</p> <p><span class="outMonth">${months} </span>months</p> <p><span class="outDay">${days} </span>days</p>`;
-    
 }
